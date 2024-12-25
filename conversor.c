024068d166694746631d1919da4f618c347c8582
Hugo S. Dias <39714844+hsantosdias@users.noreply.github.com>
2024-12-25 19:09:24 -0300

    Função temperatura() introduzida ao main()

--- conversor.c
+++ conversor.c
@@ -2,6 +2,7 @@
 #include <string.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include "interfacetemperatura.h"
 
 void comprimento() {
   double data, result;
@@ -374,7 +375,8 @@ int main() {
     break;
 /*João Vitor Soares Amorim*/ 
   case 4:
-    /* code */
+    temperatura();
+    /* Hugo Santos Dias */
     break;
 
   case 5:
