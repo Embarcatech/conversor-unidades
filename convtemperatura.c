49419fe34480d1f4b5738ee7c35941be016ab1c5
Hugo S. Dias <39714844+hsantosdias@users.noreply.github.com>
2024-12-25 09:06:26 -0300

    Formulas - para a conversão de temperaturas

--- convtemperatura.c
+++ convtemperatura.c
@@ -9,4 +9,34 @@ Arquivo inicial para a conversão de temperaturas
 
 */
 
+/*
+Fórmulas matemáticas para calcular temperaturas:
+
+Converter Celsius para Kelvin
+TK = TC + 273, onde TK é a temperatura em Kelvin e TC é a temperatura em graus Celsius. 
+Por exemplo, para converter 40° C em K, a fórmula é TK = 40 + 273, resultando em TK = 313. 
+
+Converter Fahrenheit para Celsius
+C = 5/9 x (F – 32), onde C é a temperatura em graus Celsius e F é a temperatura em graus Fahrenheit. 
+
+Converter Celsius para Fahrenheit
+F = (9/5) x TC + 32, onde F é a temperatura em Fahrenheit e TC é a temperatura em graus Celsius. 
+Por exemplo, para converter 40°C para Fahrenheit, temos: F = (9/5) x 40 + 32 = 72 + 32 = 104°F.
+
+*/
+
+#include <stdio.h>
+
+int main()
+
+{
+
+printf("Escolha a unidade de temperatura que deseja converter:\n");
+
+
+return 0;
+
+}
+
+
 
